bc0c51366ddb56c93e4b7a5f2fe87a919bd65ece
Aetherinox <aetherinox@proton.me>
2025-09-03 23:54:20 -0700

    chore: deprecate redux build
    Former-commit-id: c0768bf34a494377ad1ecbe2171441a6fdcd360d
    Former-commit-id: 462d95a47547d16875b50e84143cbbf525fa5779
    Former-commit-id: 89f5950e6d29d22b2f648d6b763a440bfa0598c7 [formerly d5e4147503871ebb5d5ffe4fd8978020d553dcae [formerly 94452c15b6d95a99ac861dea3c5be8dc34560d5c]]
    Former-commit-id: bd3353af49506c6c488ac0fa568fff2c0c7b4f20
    Former-commit-id: 4b0227fe8c5753f5f1fb0c9ca1682c180dd8a0e4

--- src/redux/cyberpanel/configservercsf/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import url
-from . import views
-
-urlpatterns = [
-
-    url(r'^$', views.configservercsf, name='configservercsf'),
-    url(r'^iframe/$', views.configservercsfiframe, name='configservercsfiframe'),
-]
--- src/redux/da/scripts/install.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-echo "This plugin is installed by installing csf from the root shell"
-
-exit 0;
