c0768bf34a494377ad1ecbe2171441a6fdcd360d
Aetherinox <aetherinox@proton.me>
2025-09-03 23:54:20 -0700

    chore: deprecate redux build

--- src/redux/da/scripts/uninstall.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-echo "This plugin is uninstalled by uninstalling csf from the root shell"
-
-exit 0;
--- src/redux/lfdcron.directadmin.sh
+++
@@ -1,2 +0,0 @@
-SHELL=/bin/sh
-0 0 * * * root /usr/sbin/csf --lfd restart > /dev/null 2>&1
