663dadc8a4286c3d66e38f4d6fa2146ace10c38c
Aetherinox <aetherinox@proton.me>
2025-09-08 02:08:14 -0700

    feat: re-enable automatic updates with new endpoint
    csf v15.10 will include automatic updates downloaded from this repository
    
    
    Former-commit-id: 955386344f8d6fe0749dc3a77438122e575a0f21
    Former-commit-id: 9790d15c2018b79292b773c239eec56a3f6060be
    Former-commit-id: ec94f0d656e16533208d98b8bdf35980f67397f5 [formerly 6a6708dba08acc1f307ac9d8f582002df9edd47e]
    Former-commit-id: bb1f026cb12d9234c1faa30420ca920cf720f415

--- src/ConfigServer/ServerCheck.pm
+++ src/ConfigServer/ServerCheck.pm
@@ -1,21 +1,23 @@
 # #
-#	  @author                   Copyright (C) 2025 Aetherinox
-#                               Copyright (C) 2006-2025 Jonathan Michaelson
-#	  @repo_primary             https://github.com/Aetherinox/csf-firewall/actions
-#	  @repo_legacy              https://github.com/waytotheweb/scripts
+#   @app                        ConfigServer Firewall (csf)
+#	@author						Copyright (C) 2025 Aetherinox
+#								Copyright (C) 2006-2025 Jonathan Michaelson
+#   @license                    GPLv3
+#	@repo_primary             	https://github.com/Aetherinox/csf-firewall/actions
+#	@repo_legacy              	https://github.com/waytotheweb/scripts
 #
-#	  This program is free software; you can redistribute it and/or modify it under
-#	  the terms of the GNU General Public License as published by the Free Software
-#	  Foundation; either version 3 of the License, or (at your option) any later
-#	  version.
+#	This program is free software; you can redistribute it and/or modify it under
+#	the terms of the GNU General Public License as published by the Free Software
+#	Foundation; either version 3 of the License, or (at your option) any later
+#	version.
 #
-#	  This program is distributed in the hope that it will be useful, but WITHOUT
-#	  ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
-#	  FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
-#	  details.
+#	This program is distributed in the hope that it will be useful, but WITHOUT
+#	ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+#	FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+#	details.
 #
-#	  You should have received a copy of the GNU General Public License along with
-#	  this program; if not, see <https://www.gnu.org/licenses>.
+#	You should have received a copy of the GNU General Public License along with
+#	this program; if not, see <https://www.gnu.org/licenses>.
 # #
 
 package ConfigServer::ServerCheck;
