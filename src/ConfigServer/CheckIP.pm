335c00642587475ea02e38a822df7d2a3044a202
Aetherinox <aetherinox@proton.me>
2025-09-26 18:44:11 -0700

    refactor: update headers, commenting in config files, line length conformity
    ensure lines are not too long as to not wrap within CSF.
    
    Format comments to make them easier to read for users

--- src/ConfigServer/CheckIP.pm
+++ src/ConfigServer/CheckIP.pm
@@ -1,22 +1,31 @@
-###############################################################################
-# Copyright (C) 2006-2025 Jonathan Michaelson
-#
-# https://github.com/waytotheweb/scripts
-#
-# This program is free software; you can redistribute it and/or modify it under
-# the terms of the GNU General Public License as published by the Free Software
-# Foundation; either version 3 of the License, or (at your option) any later
-# version.
-#
-# This program is distributed in the hope that it will be useful, but WITHOUT
-# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
-# FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
-# details.
-#
-# You should have received a copy of the GNU General Public License along with
-# this program; if not, see <https://www.gnu.org/licenses>.
-###############################################################################
+# #
+#   @app                ConfigServer Firewall & Security (CSF)
+#                       Login Failure Daemon (LFD)
+#   @website            https://configserver.dev
+#   @docs               https://docs.configserver.dev
+#   @download           https://download.configserver.dev
+#   @repo               https://github.com/Aetherinox/csf-firewall
+#   @copyright          Copyright (C) 2025-2026 Aetherinox
+#                       Copyright (C) 2006-2025 Jonathan Michaelson
+#                       Copyright (C) 2006-2025 Way to the Web Ltd.
+#   @license            GPLv3
+#   @updated            09.26.2025
+#   
+#   This program is free software; you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation; either version 3 of the License, or (at
+#   your option) any later version.
+#   
+#   This program is distributed in the hope that it will be useful, but
+#   WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+#   General Public License for more details.
+#   
+#   You should have received a copy of the GNU General Public License
+#   along with this program; if not, see <https://www.gnu.org/licenses>.
+# #
 ## no critic (RequireUseWarnings, ProhibitExplicitReturnUndef, ProhibitMixedBooleanOperators, RequireBriefOpen)
+
 # start main
 package ConfigServer::CheckIP;
 
@@ -155,6 +164,5 @@ sub cccheckip {
 	return $ret;
 }
 # end cccheckip
-###############################################################################
 
 1;
--- src/ConfigServer/ServerCheck.pm
+++ src/ConfigServer/ServerCheck.pm
@@ -1,23 +1,28 @@
 # #
-#   @app                        ConfigServer Firewall (csf)
-#	@author						Copyright (C) 2025 Aetherinox
-#								Copyright (C) 2006-2025 Jonathan Michaelson
-#   @license                    GPLv3
-#	@repo_primary             	https://github.com/Aetherinox/csf-firewall/actions
-#	@repo_legacy              	https://github.com/waytotheweb/scripts
-#
-#	This program is free software; you can redistribute it and/or modify it under
-#	the terms of the GNU General Public License as published by the Free Software
-#	Foundation; either version 3 of the License, or (at your option) any later
-#	version.
-#
-#	This program is distributed in the hope that it will be useful, but WITHOUT
-#	ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
-#	FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
-#	details.
-#
-#	You should have received a copy of the GNU General Public License along with
-#	this program; if not, see <https://www.gnu.org/licenses>.
+#   @app                ConfigServer Firewall & Security (CSF)
+#                       Login Failure Daemon (LFD)
+#   @website            https://configserver.dev
+#   @docs               https://docs.configserver.dev
+#   @download           https://download.configserver.dev
+#   @repo               https://github.com/Aetherinox/csf-firewall
+#   @copyright          Copyright (C) 2025-2026 Aetherinox
+#                       Copyright (C) 2006-2025 Jonathan Michaelson
+#                       Copyright (C) 2006-2025 Way to the Web Ltd.
+#   @license            GPLv3
+#   @updated            09.26.2025
+#   
+#   This program is free software; you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation; either version 3 of the License, or (at
+#   your option) any later version.
+#   
+#   This program is distributed in the hope that it will be useful, but
+#   WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+#   General Public License for more details.
+#   
+#   You should have received a copy of the GNU General Public License
+#   along with this program; if not, see <https://www.gnu.org/licenses>.
 # #
 
 package ConfigServer::ServerCheck;
