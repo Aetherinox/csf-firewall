78c75b9898c582863a3019cce89da5ec4b075dbc
Aetherinox <aetherinox@proton.me>
2025-10-02 12:53:59 -0700

    chore(pre/post): update output when pre.d and post.d scripts are loaded into csf

--- src/csfpre.sh
+++ src/csfpre.sh
@@ -1,13 +1,40 @@
 #!/bin/bash
+# #
+#   @app                ConfigServer Firewall & Security (CSF)
+#                       Login Failure Daemon (LFD)
+#   @website            https://configserver.dev
+#   @docs               https://docs.configserver.dev
+#   @download           https://download.configserver.dev
+#   @repo               https://github.com/Aetherinox/csf-firewall
+#   @copyright          Copyright (C) 2025-2026 Aetherinox
+#                       Copyright (C) 2006-2025 Jonathan Michaelson
+#                       Copyright (C) 2006-2025 Way to the Web Ltd.
+#   @license            GPLv3
+#   @updated            10.02.2025
+#   
+#   This program is free software; you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation; either version 3 of the License, or (at
+#   your option) any later version.
+#   
+#   This program is distributed in the hope that it will be useful, but
+#   WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+#   General Public License for more details.
+#   
+#   You should have received a copy of the GNU General Public License
+#   along with this program; if not, see <https://www.gnu.org/licenses>.
+# #
 
 CSFPRED_PATH="/usr/local/include/csf/pre.d"
 
-if [ -d ${CSFPRED_PATH} ]; then
-	for i in ${CSFPRED_PATH}/*.sh; do
-		if [ -r $i ]; then
-			. $i
-		fi
-	done
+if [ -d "${CSFPRED_PATH}" ]; then
+    for i in "${CSFPRED_PATH}"/*.sh; do
+        if [ -r "$i" ]; then
+            echo "Loading pre-script: $i"
+            . "$i"
+        fi
+    done
 
-	unset i
+    unset i
 fi
