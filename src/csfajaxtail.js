7a98415794a708007639ff6d7f6f9ff16a6a8275
Aetherinox <aetherinox@proton.me>
2025-10-04 03:08:42 -0700

    refactor: re-write csf ajax library; update csfTimer features
    csf timer now displays status when paused or restarted by user
    
    
    Former-commit-id: 113f6698fb1af5ff2568698d06912bc325c8e726
    Former-commit-id: 4b09e9183f34bb3598d51c6d0801fe4e6db87faf

--- src/csfajaxtail.js
+++ src/csfajaxtail.js
@@ -1,145 +1,282 @@
-//#############################################################################
-//# Copyright (C) 2006-2025 Jonathan Michaelson
-//#
-//# https://github.com/waytotheweb/scripts
-//#
-//# This program is free software; you can redistribute it and/or modify it under
-//# the terms of the GNU General Public License as published by the Free Software
-//# Foundation; either version 3 of the License, or (at your option) any later
-//# version.
-//#
-//# This program is distributed in the hope that it will be useful, but WITHOUT
-//# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
-//# FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
-//# details.
-//#
-//# You should have received a copy of the GNU General Public License along with
-//# this program; if not, see <https://www.gnu.org/licenses>.
-//#############################################################################
+/*
+    @app                ConfigServer Firewall & Security (CSF)
+                        Login Failure Daemon (LFD)
+    @website            https://configserver.dev
+    @docs               https://docs.configserver.dev
+    @download           https://download.configserver.dev
+    @repo               https://github.com/Aetherinox/csf-firewall
+    @copyright          Copyright (C) 2025-2026 Aetherinox
+                        Copyright (C) 2006-2025 Jonathan Michaelson
+                        Copyright (C) 2006-2025 Way to the Web Ltd.
+    @license            GPLv3
+    @updated            09.26.2025
+    
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 3 of the License, or (at
+    your option) any later version.
+    
+    This program is distributed in the hope that it will be useful, but
+    WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+    General Public License for more details.
+    
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, see <https://www.gnu.org/licenses>.
+*/
 
-var CSFscript = '';
-var CSFcountval = typeof CSFcountval !== 'undefined' ? CSFcountval : 6;
-var CSFlineval = 100;
-var CSFcounter;
-var CSFcount = 1;
-var CSFpause = 0;
-var CSFfrombot = 120;
-var CSFfromright = 10;
-var CSFsettimer = 0;
-var CSFheight = 0;
-var CSFwidth = 0;
-var CSFajaxHTTP = CSFcreateRequestObject();
+var csfScript = '';
+var csfCountVal = typeof csfCountVal !== 'undefined' ? csfCountVal : 6;
+var csfCounter;
+var csfCount = 1;
+var csfPause = 0;
+var csfFromBot = 120;
+var csfFromRight = 10;
+var csfTimerSet = 0;
+var csfHeight = 0;
+var csfWidth = 0;
+var csfAjaxHttp = csfCreateReqObject( );
 
-function CSFcreateRequestObject() {
-	var CSFajaxRequest;
-	if (window.XMLHttpRequest) {
-		CSFajaxRequest = new XMLHttpRequest();
-	}
-	else if (window.ActiveXObject) {
-		CSFajaxRequest = new ActiveXObject("Microsoft.XMLHTTP");
-	}
-	else {
-		alert('There was a problem creating the XMLHttpRequest object in your browser');
-		CSFajaxRequest = '';
-	}
-	return CSFajaxRequest;
+/*
+    Creates and returns a compatible XMLHttpRequest object
+*/
+
+function csfCreateReqObject( )
+{
+    var csfAjaxReq;
+
+    if ( window.XMLHttpRequest )
+    {
+        csfAjaxReq = new XMLHttpRequest( );
+    }
+    else if ( window.ActiveXObject )
+    {
+        csfAjaxReq = new ActiveXObject( 'Microsoft.XMLHTTP' );
+    }
+    else
+    {
+        alert( 'There was a problem creating the XMLHttpRequest object in your browser' );
+        csfAjaxReq = '';
+    }
+
+    return csfAjaxReq;
 }
 
-function CSFsendRequest(url) {
-	var now = new Date();
-	CSFajaxHTTP.open('get', url + '&nocache=' + now.getTime());
-	CSFajaxHTTP.onreadystatechange = CSFhandleResponse;
-	CSFajaxHTTP.send();
-	document.getElementById("CSFrefreshing").style.display = "inline";
-} 
+/*
+    Sends an asynchronous GET request to the specified URL
+*/
 
-function CSFhandleResponse() {
-	if(CSFajaxHTTP.readyState == 4 && CSFajaxHTTP.status == 200){
-		if(CSFajaxHTTP.responseText) {
-			var CSFobj = document.getElementById("CSFajax");
-			CSFobj.innerHTML = CSFajaxHTTP.responseText;
-			waitForElement("CSFajax",function(){
-				CSFobj.scrollTop = CSFobj.scrollHeight;
-			});
-			document.getElementById("CSFrefreshing").style.display = "none";
-			if (CSFsettimer) {CSFcounter = setInterval(CSFtimer, 1000);}
-		}
-	}
+function csfSendReq( url )
+{
+    var now = new Date( );
+
+    csfAjaxHttp.open( 'get', url + '&nocache=' + now.getTime( ) );
+    csfAjaxHttp.onreadystatechange = csfHandleResp;
+    csfAjaxHttp.send( );
+
+    document.getElementById( 'csfRefreshing' ).style.display = 'inline';
 }
 
-function waitForElement(elementId, callBack){
-	window.setTimeout(function(){
-		var element = document.getElementById(elementId);
-		if(element){
-			callBack(elementId, element);
-		}else{
-			waitForElement(elementId, callBack);
-		}
-	},500)
+/*
+    Handles and processes the ajax response from the server
+*/
+
+function csfHandleResp( )
+{
+    if ( csfAjaxHttp.readyState == 4 && csfAjaxHttp.status == 200 )
+    {
+        if ( csfAjaxHttp.responseText )
+        {
+            var csfObj = document.getElementById( 'csfAjax' );
+            csfObj.innerHTML = csfAjaxHttp.responseText;
+
+            waitForElement( 'csfAjax', function( )
+            {
+                csfObj.scrollTop = csfObj.scrollHeight;
+            });
+
+            document.getElementById( 'csfRefreshing' ).style.display = 'none';
+
+            if ( csfTimerSet )
+            {
+                csfCounter = setInterval( csfTimerInitialize, 1000 );
+            }
+        }
+    }
 }
 
-function CSFgrep() {
-	CSFsettimer = 0;
-	var CSFlogobj = document.getElementById("CSFlognum");
-	var CSFlognum;
-	if (CSFlogobj) {CSFlognum = '&lognum=' + CSFlogobj.options[CSFlogobj.selectedIndex].value}
-	else {CSFlognum = ""}
-	if (document.getElementById("CSFgrep_i").checked) {CSFlognum = CSFlognum + "&grepi=1"}
-	if (document.getElementById("CSFgrep_E").checked) {CSFlognum = CSFlognum + "&grepE=1"}
-	if (document.getElementById("CSFgrep_Z").checked) {CSFlognum = CSFlognum + "&grepZ=1"}
-	var CSFurl = CSFscript + '&grep=' + document.getElementById("CSFgrep").value + CSFlognum;
-	CSFsendRequest(CSFurl);
+/*
+    Waits for an element to exist in the DOM, then executes a callback
+*/
+
+function waitForElement( elementId, callBack )
+{
+    window.setTimeout( function( )
+    {
+        var element = document.getElementById( elementId );
+
+        if ( element )
+        {
+            callBack( elementId, element );
+        }
+        else
+        {
+            waitForElement( elementId, callBack );
+        }
+    }, 500 );
 }
 
-function CSFtimer() {
-	CSFsettimer = 1;
-	if (CSFpause) {return}
-	CSFcount = CSFcount - 1;
-	document.getElementById("CSFtimer").innerHTML = CSFcount;
-	if (CSFcount <= 0) {
-		clearInterval(CSFcounter);
-		var CSFlogobj = document.getElementById("CSFlognum");
-		var CSFlognum;
-		if (CSFlogobj) {CSFlognum = '&lognum=' + CSFlogobj.options[CSFlogobj.selectedIndex].value}
-		else {CSFlognum = ""}
-		CSFsendRequest(CSFscript + '&lines=' + document.getElementById("CSFlines").value + CSFlognum);
-		CSFcount = CSFcountval;
-		return;
-	}
+/*
+    Handles log grep requests using user input and selected options
+*/
+
+function csfGrep( )
+{
+    csfTimerSet = 0;
+
+    var csfLogObj = document.getElementById( 'csfLogNum' );
+    var csfLogNum;
+
+    if ( csfLogObj )
+    {
+        csfLogNum = '&lognum=' + csfLogObj.options[ csfLogObj.selectedIndex ].value;
+    }
+    else
+    {
+        csfLogNum = '';
+    }
+
+    if ( document.getElementById( 'CSFgrep_i' ).checked )
+    {
+        csfLogNum += '&grepi=1';
+    }
+
+    if ( document.getElementById( 'CSFgrep_E' ).checked )
+    {
+        csfLogNum += '&grepE=1';
+    }
+
+    if ( document.getElementById( 'CSFgrep_Z' ).checked )
+    {
+        csfLogNum += '&grepZ=1';
+    }
+
+    var csfUrl = csfScript + '&grep=' + document.getElementById( 'csfGrep' ).value + csfLogNum;
+    csfSendReq( csfUrl );
 }
 
-function CSFpausetimer() {
-	if (CSFpause) {
-		CSFpause = 0;
-		document.getElementById("CSFpauseID").innerHTML = "Pause";
-	}
-	else {
-		CSFpause = 1;
-		document.getElementById("CSFpauseID").innerHTML = "Continue";
-	}
+/*
+    Timer › Initialize
+    Automatically refreshes on-screen logs at regular intervals
+*/
+
+function csfTimerInitialize( ) 
+{
+    csfTimerSet = 1;
+
+    const timerEl = document.getElementById( 'csfTimer' );
+    if ( !timerEl ) return;
+
+    /*
+        If paused, show "Paused" instead of the count
+    */
+    if ( csfPause )
+    {
+        timerEl.textContent = 'Paused';
+        return;
+    }
+
+    /*
+        Decrement timer and update display
+    */
+    csfCount--;
+    timerEl.textContent = csfCount;
+
+    /*
+        When timer hits zero, reset and perform request
+    */
+    if ( csfCount <= 0 )
+    {
+        clearInterval( csfCounter );
+
+        const logObj = document.getElementById( 'csfLogNum' );
+        const linesVal = document.getElementById( 'csfLines' ).value;
+        const logNum = logObj ? `&lognum=${ logObj.value }` : '';
+
+        csfSendReq( `${ csfScript }&lines=${ linesVal }${ logNum }` );
+        csfCount = csfCountVal;
+    }
 }
 
-function CSFrefreshtimer() {
-	var pause = CSFpause;
-	CSFcount = 1;
-	CSFpause = 0;
-	CSFtimer();
-	CSFpause = pause;
-	CSFcount = CSFcountval - 1;
-	document.getElementById("CSFtimer").innerHTML = CSFcount;
+/*
+    Timer › Pause
+    Toggles the automatic refresh pause state and updates button text
+*/
+
+function csfTimerPause( )
+{
+    /*
+        Toggle pause state
+    */
+
+    csfPause = csfPause ? 0 : 1;
+
+    /*
+        Update button label
+    */
+
+    const pauseBtn = document.getElementById( 'csfPauseId' );
+    if ( pauseBtn )
+    {
+        pauseBtn.textContent = csfPause ? 'Continue' : 'Pause';
+    }
 }
 
-function windowSize() {
-	if( typeof( window.innerHeight ) == 'number' ) {
-		CSFheight = window.innerHeight;
-		CSFwidth = window.innerWidth;
-	}
-	else if (document.documentElement && (document.documentElement.clientHeight)) {
-		CSFheight = document.documentElement.clientHeight;
-		CSFwidth = document.documentElement.clientWidth;
-	}
-	else if (document.body && (document.body.clientHeight)) {
-		CSFheight = document.body.clientHeight;
-		CSFwidth = document.body.clientWidth;
-	}
+/*
+    Timer › Refresh
+    Forces an immediate refresh without waiting for timer to expire
+*/
+
+function csfTimerRefresh( ) 
+{
+    /*
+        Temporarily unpause and run a one-time timer tick
+    */
+
+    const prevPause = csfPause;
+    csfPause = 0;
+    csfCount = 1;
+    csfTimerInitialize( );
+    csfPause = prevPause;
+
+    /*
+        Reset counter and update display
+    */
+
+    csfCount = csfCountVal - 1;
+    const timerEl = document.getElementById( 'csfTimer' );
+    if ( timerEl ) timerEl.textContent = csfCount;
+}
+
+/*
+    Gets and stores the current browser window width and height
+*/
+
+function windowSize( )
+{
+    if ( typeof( window.innerHeight ) == 'number' )
+    {
+        csfHeight = window.innerHeight;
+        csfWidth = window.innerWidth;
+    }
+    else if ( document.documentElement && ( document.documentElement.clientHeight ) )
+    {
+        csfHeight = document.documentElement.clientHeight;
+        csfWidth = document.documentElement.clientWidth;
+    }
+    else if ( document.body && ( document.body.clientHeight ) )
+    {
+        csfHeight = document.body.clientHeight;
+        csfWidth = document.body.clientWidth;
+    }
 }
