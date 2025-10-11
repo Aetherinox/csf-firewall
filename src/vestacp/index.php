9b9d421cc1b9ea0aa127525584f0f4ec5d716e04
Aetherinox <aetherinox@proton.me>
2025-10-10 19:45:25 -0700

    fix(vestacp): re-implement header into csf management page
    After VestaCP react  v1.0 which migrated the app to React, header no longer shows on `/list/csf` page.
    
    This re-adds the header so that users can navigate around

--- src/vestacp/index.php
+++ src/vestacp/index.php
@@ -1,16 +1,22 @@
+<link rel="stylesheet" href="<?php $_SERVER['DOCUMENT_ROOT'] ?>/css/styles.min.css">
+
 <?php
-error_reporting(NULL);
 /*
     VestaCP migrated to React in their v1.0 update.
     All pages are pre-compiled into .js / .css bundle and then pushed
     to the VestaCP repo.
 */
 
+error_reporting(NULL);
+
 $TAB = 'CSF';
 
 include($_SERVER['DOCUMENT_ROOT']."/inc/main.php");
+include($_SERVER['DOCUMENT_ROOT'].'/templates/admin/panel.html');
+top_panel(empty($_SESSION['look']) ? $_SESSION['user'] : $_SESSION['look'], $TAB);
 
-if ($_SESSION['user'] != 'admin') {
+if ($_SESSION['user'] != 'admin')
+{
     header("Location: /list/user");
     exit;
 }
@@ -22,18 +28,20 @@ top_panel(empty($_SESSION['look']) ? $_SESSION['user'] : $_SESSION['look'], $TAB
     <div class="l-separator"></div>
     <!-- /.l-separator -->
 	<div class="l-center units">
-<iframe scrolling='auto' name='myiframe' id='myiframe' src='frame.php' frameborder='0' width='100%' onload='resizeIframe(this);'></iframe>
-<script>
-function resizeIframe(obj) {
- obj.style.height = obj.contentWindow.document.body.scrollHeight + 'px';
- window.scrollTo(0,0);
- window.parent.scrollTo(0,0);
- window.parent.parent.scrollTo(0,0);
-}
-</script>
-</div>
-<?php
+        <iframe scrolling='auto' name='myiframe' id='myiframe' src='frame.php' frameborder='0' width='100%' onload='resizeIframe(this);'></iframe>
 
+        <script>
+            function resizeIframe( obj ) 
+            {
+                obj.style.height = obj.contentWindow.document.body.scrollHeight + 'px';
+                window.scrollTo(0,0);
+                window.parent.scrollTo(0,0);
+                window.parent.parent.scrollTo(0,0);
+            }
+        </script>
+    </div>
+
+<?php
 $_SESSION['back'] = $_SERVER['REQUEST_URI'];
 
 include($_SERVER['DOCUMENT_ROOT'].'/templates/scripts.html');
