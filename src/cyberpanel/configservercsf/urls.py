be651d9c7425f10b79128dfc924c2a4d36ac729d
Aetherinox <aetherinox@proton.me>
2025-10-11 04:37:14 -0700

    fix(cyberpanel): update class & module imports
    Former-commit-id: 4127813c4e53ee0da4df27f0e21d8de35f54efac
    Former-commit-id: 5bde4929b1bb3af2aaca6cf2792eecc46808fbf9

--- src/cyberpanel/configservercsf/urls.py
+++ src/cyberpanel/configservercsf/urls.py
@@ -1,8 +1,7 @@
-from django.conf.urls import url
+from django.urls import path
 from . import views
 
 urlpatterns = [
-
-    url(r'^$', views.configservercsf, name='configservercsf'),
-    url(r'^iframe/$', views.configservercsfiframe, name='configservercsfiframe'),
+    path('', views.configservercsf, name='configservercsf'),
+    path('iframe', views.configservercsfiframe, name='configservercsfiframe'),
 ]
