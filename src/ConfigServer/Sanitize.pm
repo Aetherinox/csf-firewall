c49c363e583ad09335e99a6913b4b928eadea2a2
Aetherinox <aetherinox@proton.me>
2026-02-25 21:09:50 -0700

    feat(deps): update `Sanitize` module subs

--- src/ConfigServer/Sanitize.pm
+++ src/ConfigServer/Sanitize.pm
@@ -9,7 +9,7 @@
 #                       Copyright (C) 2006-2025 Jonathan Michaelson
 #                       Copyright (C) 2006-2025 Way to the Web Ltd.
 #   @license            GPLv3
-#   @updated            02.19.2026
+#   @updated            02.25.2026
 #   
 #   This program is free software; you can redistribute it and/or modify
 #   it under the terms of the GNU General Public License as published by
@@ -24,16 +24,51 @@
 #   You should have received a copy of the GNU General Public License
 #   along with this program; if not, see <https://www.gnu.org/licenses>.
 # #
+
+=head1 NAME
+
+ConfigServer::Sanitize - Sanitize strings and html output
+
+=head1 TESTING
+
+Compare uri_escape implementations:
+
+	perl -I "/usr/local/csf/lib" -e '
+	use ConfigServer::Sanitize ();
+	use ConfigServer::Perl::URI ();
+
+	my $test = "sanitize=config&server fw";
+
+	print "Sanitize: " . ConfigServer::Sanitize::uri_escape($test) . "\n";
+	print "URI:      " . ConfigServer::Perl::URI::uri_escape($test) . "\n";
+
+	print(
+		ConfigServer::Sanitize::uri_escape($test)
+			eq ConfigServer::Perl::URI::uri_escape($test)
+		? "MATCH\n"
+		: "MISMATCH\n"
+	);
+	'
+
+
+=cut
+
+# #
+#	@package		ConfigServer::Sanitize
+#	@desc			In-house uri_sanitize
+# #
+
 ## no critic (RequireUseWarnings, ProhibitExplicitReturnUndef)
 package ConfigServer::Sanitize;
 
 use strict;
 use lib '/usr/local/csf/lib';
+use Carp;
 
-use Exporter qw(import);
-our $VERSION	= 1.00;
-our @ISA		= qw(Exporter);
-our @EXPORT_OK	= qw(html_safe html_escape);
+use Exporter 		qw(import);
+our $VERSION 		= '5.35';
+our @ISA			= qw(Exporter);
+our @EXPORT_OK		= qw(html_safe html_escape uri_escape);
 
 # #
 #   Sanitize › HTML › Whitelisted Tags / Attribs
@@ -41,11 +76,12 @@ our @EXPORT_OK	= qw(html_safe html_escape);
 #	Tag only (attrib => 1)		allow tags only; no attribs.
 #	Tags w/ attrib hash			allow tags with specific attribs.
 #	
+#	@assoc		ConfigServer::Sanitize::html_safe
 #	@usage		b => 1,							'bold' tag alloweed; no attribs allowed
 #				span => { class => 1 },			'span' tag allowed; 'class' attrib allowed
 # #
 
-my %allowed_tags = (
+my %HTML_WHITELIST_TAGS = (
 	b		=> 1,
 	u		=> 1,
 	i		=> 1,
@@ -56,16 +92,18 @@ my %allowed_tags = (
 );
 
 # #
-#   Sanitize › HTML › Escape
+#   Sanitize › HTML › Safe
 #	
-#	When displaying information on the settings page; certain settings may contain
-#	html for emphasis. 
+#	When displaying information on the settings page; certain settings may contain 
+#	html for emphasis.  
+#	 
+#	Escapes html sensitive chars ( &, <, > ), and ensures only certain  
+#		tags and attribs are allowed. onclick, onerror, etc are stripped. 
+#	Restores tags / attribs listed in HTML_WHITELIST_TAGS
 #	
-#	This sub escapes html sensitive chars ( &, <, > ), and ensures only certain 
-#		tags and attribs are allowed. onclick, onerror, etc are stripped.
-#	After filtering the setting description, only the safe tags and attribs remain.
-#	
-#   @usage      my $safe = html_safe( $unsafe_html );
+#	@package	ConfigServer::Sanitize
+#   @usage      my $safe = html_safe( "<b>bold</b> <script>alert(1)</script>" );
+#					# returns: <b>bold</b> &lt;script&gt;alert(1)&lt;/script&gt;
 #   @scope      public
 #   @param      line        str                     text to sanitize
 #   @return                 str                     sanitized text
@@ -87,9 +125,9 @@ sub html_safe
     #	restore allowed tags
 	# #
 
-    foreach my $tag ( keys %allowed_tags )
+    foreach my $tag ( keys %HTML_WHITELIST_TAGS )
 	{
-		my $allowed = $allowed_tags{$tag};
+		my $allowed = $HTML_WHITELIST_TAGS{$tag};
 		
 		if ( ref($allowed) eq 'HASH' )
 		{
@@ -164,26 +202,66 @@ sub html_safe
 # #
 #   Sanitize › HTML › Escape
 #	
-#   Escape html before output.
-#	Utilized to output setting descriptions on "Firewall Configuration" page where html can
-#		be present.
-#   
-#   @usage      my $safe_html = html_escape( $untrusted );
-#   
-#   @param      text        str         html to escape
-#   @return                 str         escaped text safe for HTML attributes
+#   Escape all HTML-sensitive chars (&, <, >, ", ') into their entity variant.
+#	Unlike html_safe(), no tags are restored; everything is escaped.
+#	
+#	@assoc		ConfigServer::Sanitize::html_escape
+#	@package	ConfigServer::Sanitize
+#   @usage      my $safe = html_escape( '<script>alert(1)</script>' );
+#					# returns: &lt;script&gt;alert(1)&lt;/script&gt;
+#   @scope      public
+#   @param      text        str        				html to escape
+#   @return                 str         			escaped text safe for HTML attributes
 # #
+
+my %HTML_ESCAPE_MAP = (
+	'&' => '&amp;',
+	'<' => '&lt;',
+	'>' => '&gt;',
+	'"' => '&quot;',
+	"'" => '&#39;'
+);
 
 sub html_escape
 {
-    my ($text) = @_;
+    my ( $text ) = @_;
     return '' unless defined $text;
-    $text =~ s/&/&amp;/g;
-    $text =~ s/</&lt;/g;
-    $text =~ s/>/&gt;/g;
-    $text =~ s/"/&quot;/g;
-    $text =~ s/'/&#39;/g;
+    return $text if $text !~ tr/&<>"'//;
+
+    $text =~ s/([&<>"'])/$HTML_ESCAPE_MAP{$1}/sg;
+
     return $text;
+}
+
+# #
+#   Sanitize › URI › Escape
+#	
+#   Percent-encode strings for use in URI. All chars except RFC 3986 
+#	unreserved chars (A-Za-z0-9 - . _ ~) are encoded as %XX hex pairs.
+#	Handles UTF-8 flagged strings by encoding to bytes first.
+#	
+#   In-house replacement for perl package URI::Escape::uri_escape; no external deps.
+#	
+#	@package	ConfigServer::Sanitize
+#	@ref		https://metacpan.org/pod/URI::Escape
+#				https://raw.githubusercontent.com/libwww-perl/URI/185b40aabcef277d7dffdd2ba7c1c38f00302084/lib/URI/Escape.pm
+#   @usage      use ConfigServer::Sanitize qw(uri_escape);
+#					my $encoded = uri_escape( "configserver=foo&bar" );
+#					# returns: configserver%3Dfoo%26bar
+#   @scope      public
+#   @param      str         str         			string to encode
+#   @return                 str         			percent-encoded string
+# #
+
+sub uri_escape
+{
+	my ( $str ) = @_;
+	return undef unless defined $str;
+
+	utf8::encode( $str ) if utf8::is_utf8( $str );
+	$str =~ s/([^A-Za-z0-9\-\.\_\~])/sprintf( "%%%02X", ord( $1 ) )/ge;
+
+	return $str;
 }
 
 1;
