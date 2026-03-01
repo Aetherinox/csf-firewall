1754a2ec710c47d8d0be4f507904f3a2129dcc50
Aetherinox <aetherinox@proton.me>
2026-03-01 16:28:40 -0700

    refactor(webui): condense retry indicator for login attempts

--- src/lfd.pl
+++ src/lfd.pl
@@ -10740,22 +10740,13 @@ EOF
 
 						if ($valid eq "fail" || $valid eq "login-retry")
 						{
-							if ( $config{UI_RETRY_SHOW_REMAINING} == 1 )
-							{
-								print "<div class='login-notify-failure'>\n";
-								print "		<span class='login-notify-failure-icon'><svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 576 512'><path d='M569.517 440.013C587.975 472.007 564.806 512 527.94 512H48.054c-36.937 0-59.999-40.055-41.577-71.987L246.423 23.985c18.467-32.009 64.72-31.951 83.154 0l239.94 416.028zM288 354c-25.405 0-46 20.595-46 46s20.595 46 46 46 46-20.595 46-46-20.595-46-46-46zm-43.673-165.346l7.418 136c.347 6.364 5.609 11.346 11.982 11.346h48.546c6.373 0 11.635-4.982 11.982-11.346l7.418-136c.375-6.874-5.098-12.654-11.982-12.654h-63.383c-6.884 0-12.356 5.78-11.981 12.654z'/></svg></span>\n";
-								print "		<span class='login-notify-failure-text'>Authentication failed</span>\n";
-								print "		<span class='login-notify-failure-count'>$fails{$peeraddress} / $config{UI_RETRY}</span>\n";
-								print "</div>\n";
-							}
-							else
-							{
-								print "<div class='login-notify-failure'>\n";
-								print "		<span class='login-notify-failure-icon'><svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 576 512'><path d='M569.517 440.013C587.975 472.007 564.806 512 527.94 512H48.054c-36.937 0-59.999-40.055-41.577-71.987L246.423 23.985c18.467-32.009 64.72-31.951 83.154 0l239.94 416.028zM288 354c-25.405 0-46 20.595-46 46s20.595 46 46 46 46-20.595 46-46-20.595-46-46-46zm-43.673-165.346l7.418 136c.347 6.364 5.609 11.346 11.982 11.346h48.546c6.373 0 11.635-4.982 11.982-11.346l7.418-136c.375-6.874-5.098-12.654-11.982-12.654h-63.383c-6.884 0-12.356 5.78-11.981 12.654z'/></svg></span>\n";
-								print "		<span class='login-notify-failure-text'>Authentication failed</span>\n";
-								print "		<span class='login-notify-failure-count'></span>\n";
-								print "</div>\n";
-							}
+							my $retryAttempt = $config{UI_RETRY_SHOW_REMAINING} == 1 ? "$fails{$peeraddress} / $config{UI_RETRY}" : "";
+
+							print "<div class='login-notify-failure'>\n";
+							print "    <span class='login-notify-failure-icon'><svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 576 512'><path d='M569.517 440.013C587.975 472.007 564.806 512 527.94 512H48.054c-36.937 0-59.999-40.055-41.577-71.987L246.423 23.985c18.467-32.009 64.72-31.951 83.154 0l239.94 416.028zM288 354c-25.405 0-46 20.595-46 46s20.595 46 46 46 46-20.595 46-46-20.595-46-46-46zm-43.673-165.346l7.418 136c.347 6.364 5.609 11.346 11.982 11.346h48.546c6.373 0 11.635-4.982 11.982-11.346l7.418-136c.375-6.874-5.098-12.654-11.982-12.654h-63.383c-6.884 0-12.356 5.78-11.981 12.654z'/></svg></span>\n";
+							print "    <span class='login-notify-failure-text'>Authentication failed</span>\n";
+							print "    <span class='login-notify-failure-count'>$retryAttempt</span>\n";
+							print "</div>\n";
 						}
 
 						print "<div class='login-container-main'>";
